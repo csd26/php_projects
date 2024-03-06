87a8bcb88b173d304f238b9ac00bfcfe84bf04e2
csd26 <rvd5959@hotmail.com>
2024-03-06 09:44:09 +0700

    go

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
-echo "Hello World!"
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to the first page.</h1>
+</body>
+</html>
